c35910d5704a6f746ad88138946aef796794ec07
Florian <pfistfl@users.noreply.github.com>
2019-07-18 16:52:51 +0200

    Regression usecase (#23)
    * Add impute, html plots
    
    * Add link to vignette
    
    * Add .bibtex
    
    * Add links
    
    * Fix missing doublecarret
    
    * merge and fix for review
    
    * Fixup some stuff, remove linebreaks
    
    * Add custom pipeops
    
    * Break lines
    
    * Finalize linebreaks after sentences
    
    * More linebreaks
    
    * Start regression usecase
    
    * Merge master II
    
    * Add DESCRIPTION
    
    * Start fixing
    
    * Extend book
    
    * update
    
    * Move ext
    
    * Update book
    
    * Add PipeOp figure
    
    * Fix some more typos
    
    * Add deps
    
    * Resolve suggestions!
    
    * Update suggestions
    
    * More overlooked suggestions
    
    * Add dep 3.1.0
    
    * Add dot
    
    * move uncommented stuff to attic
    
    * Added link!
    
    * Some things I didnt catch

---
+++ R/house_sales_prediction.R
@@ -0,0 +1,13 @@
+#' Predicting House Prices
+#'
+#' This dataset contains house sale prices for King County, which includes Seattle.
+#' It includes homes sold between May 2014 and May 2015.
+#' It's a great dataset for evaluating simple regression models.
+#'
+#' @source https://www.kaggle.com/harlfoxem/housesalesprediction
+#' License: CC0: Public Domain
+#' @format A data.table with 19 house features plus the price and the id columns, 21613 observations.
+#''
+#' @examples
+#' data("house_sales_prediction", package = "mlr3book")
+"house_sales_prediction"
