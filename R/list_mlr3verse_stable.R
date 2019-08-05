d92fa113b229cf8316e11a82218309acc2e82a96
pat-s <patrick.schratz@gmail.com>
2019-08-05 22:19:37 +0200

    add list_mlr3verse_stable

---
+++ R/list_mlr3verse_stable.R
@@ -0,0 +1,21 @@
+#' @title Lists the mlr3verse pkg states of last successful mlr3book build
+#'
+list_mlr3verse_stable = function() {
+
+  pkgs = map_chr(mlr3verse::mlr3verse_packages(include_self = FALSE), function(.x) {
+
+    sessioninfo::package_info(.x) %>%
+      dplyr::filter(package == .x) %>%
+      dplyr::select(source) %>%
+      dplyr::pull() %>%
+      strsplit(split="[()]") %>%
+      unlist() %>%
+      magrittr::extract(2)
+
+  })
+
+  pkgs = na.omit(pkgs)
+
+  print(sprintf("remotes::install_github(c(%s), force = TRUE)", str_collapse(pkgs, quote = "'")), quote = FALSE)
+
+}
