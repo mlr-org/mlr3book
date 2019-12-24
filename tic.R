5b2393dcce7bae849094cda1f2dffacb8d4086af
pat-s <patrick.schratz@gmail.com>
2019-12-24 13:32:28 +0100

    try fixing deployment

--- tic.R
+++ tic.R
@@ -8,10 +8,6 @@ if (Sys.getenv("HTML") == "true") {
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::gitbook")
     ))) %>%
-    # add_step(step_run_code(unlink(dir("docs", pattern = "^[^0-9]",
-    #   full.names = TRUE), recursive = TRUE))) %>%
-    # add_step(step_run_code(fs::dir_copy(here::here("bookdown/_book"),
-    #   here::here("docs/")))) %>%
     add_step(step_run_code({
       files <- dir("bookdown/_book/", pattern = "[.]html$", full.names = TRUE)
       purrr::walk(files, ~ {
@@ -21,7 +17,7 @@ if (Sys.getenv("HTML") == "true") {
     add_step(step_add_to_known_hosts("github.com")) %>%
     add_step(step_install_ssh_keys()) %>%
     add_step(step_setup_push_deploy(path = "bookdown/_book/", branch = "gh-pages")) %>%
-    add_step(step_do_push_deploy(path = "bookdown/_book/"))
+    add_step(step_do_push_deploy(path = "bookdown/_book"))
 } else if (Sys.getenv("PDF") == "true") {
 
   get_stage("deploy") %>%
@@ -29,12 +25,8 @@ if (Sys.getenv("HTML") == "true") {
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::pdf_book")
     ))) %>%
-    # add_step(step_run_code(unlink(dir("docs", pattern = "^[^0-9]",
-    #   full.names = TRUE), recursive = TRUE))) %>%
-    # add_step(step_run_code(fs::file_copy(here::here("bookdown/_book/mlr3book.pdf"),
-    #   here::here("docs/")))) %>%
     add_step(step_add_to_known_hosts("github.com")) %>%
     add_step(step_install_ssh_keys()) %>%
     add_step(step_setup_push_deploy(path = "bookdown/_book/", branch = "gh-pages")) %>%
-    add_step(step_do_push_deploy(path = "bookdown/_book/", commit_paths = 'mlr3book.pdf'))
+    add_step(step_do_push_deploy(path = "bookdown/_book", commit_paths = 'mlr3book.pdf'))
 }
