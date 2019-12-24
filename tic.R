190ec4c9d1b56167edae5977e667ed85e4010262
pat-s <patrick.schratz@gmail.com>
2019-12-24 10:29:02 +0100

    install ssh keys

--- tic.R
+++ tic.R
@@ -31,5 +31,7 @@ if (Sys.getenv("HTML") == "true") {
 }
 
 get_stage("deploy") %>%
+  add_step(step_add_to_known_hosts("github.com")) %>%
+  add_step(step_install_ssh_keys()) %>%
   add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages")) %>%
   add_step(step_do_push_deploy(path = "docs"))
