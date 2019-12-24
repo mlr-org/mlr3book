5e74646b0da6b291920ba89e136aa0aa3226de11
pat-s <patrick.schratz@gmail.com>
2019-12-24 12:27:22 +0100

    all in deploy stage

--- tic.R
+++ tic.R
@@ -2,7 +2,8 @@ get_stage("install") %>%
   add_step(step_run_code(remotes::install_deps(dependencies = TRUE)))
 
 if (Sys.getenv("HTML") == "true") {
-  get_stage("script") %>%
+
+  get_stage("deploy") %>%
     add_step(step_run_code(withr::with_dir(
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::gitbook")
@@ -16,10 +17,14 @@ if (Sys.getenv("HTML") == "true") {
       purrr::walk(files, ~ {
         print(system.time(pkgdown::autolink_html(.x)))
       })
-    }))
+    })) %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages")) %>%
+    add_step(step_do_push_deploy(path = "docs"))
 } else if (Sys.getenv("PDF") == "true") {
 
-  get_stage("script") %>%
+  get_stage("deploy") %>%
     add_step(step_run_code(withr::with_dir(
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::pdf_book")
@@ -27,11 +32,9 @@ if (Sys.getenv("HTML") == "true") {
     add_step(step_run_code(unlink(dir("docs", pattern = "^[^0-9]",
       full.names = TRUE), recursive = TRUE))) %>%
     add_step(step_run_code(file.copy(dir("book/_book/mlr3book.pdf",
-      full.names = TRUE), "docs")))
+      full.names = TRUE), "docs"))) %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages")) %>%
+    add_step(step_do_push_deploy(path = "docs"))
 }
-
-get_stage("deploy") %>%
-  add_step(step_add_to_known_hosts("github.com")) %>%
-  add_step(step_install_ssh_keys()) %>%
-  add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages")) %>%
-  add_step(step_do_push_deploy(path = "docs"))
