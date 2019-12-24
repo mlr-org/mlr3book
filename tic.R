1a6a6fbcbe5e80ce8ccd6d86c5655a0c4b2aa769
pat-s <patrick.schratz@gmail.com>
2019-12-24 13:50:47 +0100

    next try

--- tic.R
+++ tic.R
@@ -4,6 +4,9 @@ get_stage("install") %>%
 if (Sys.getenv("HTML") == "true") {
 
   get_stage("deploy") %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_setup_push_deploy(path = here::here("bookdown/_book"), branch = "gh-pages")) %>%
     add_step(step_run_code(withr::with_dir(
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::gitbook")
@@ -14,19 +17,16 @@ if (Sys.getenv("HTML") == "true") {
         print(system.time(pkgdown::autolink_html(.x)))
       })
     })) %>%
-    add_step(step_add_to_known_hosts("github.com")) %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_setup_push_deploy(path = here::here("bookdown/_book"), branch = "gh-pages")) %>%
     add_step(step_do_push_deploy(path = here::here("bookdown/_book")))
 } else if (Sys.getenv("PDF") == "true") {
 
   get_stage("deploy") %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_setup_push_deploy(path = here::here("bookdown/_book"), branch = "gh-pages")) %>%
     add_step(step_run_code(withr::with_dir(
       "bookdown",
       bookdown::render_book("_output.yml", output_format = "bookdown::pdf_book")
     ))) %>%
-    add_step(step_add_to_known_hosts("github.com")) %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_setup_push_deploy(path = here::here("bookdown/_book"), branch = "gh-pages")) %>%
     add_step(step_do_push_deploy(path = here::here("bookdown/_book"), commit_paths = here::here("bookdown/_book/mlr3book.pdf")))
 }
